d46517a1c32fef8adddf4e31ee8392bd8c46c6d1
Virvolta <33796736+Virvolta@users.noreply.github.com>
2018-12-24 13:01:59 +0100

    Update Main.php

--- src/Seyz/EnderChestSlots/Main.php
+++ src/Seyz/EnderChestSlots/Main.php
@@ -12,31 +12,54 @@ use pocketmine\item\Item;
 
 class Main extends PluginBase implements Listener {
 	
-	public function onEnable(){
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		@mkdir($this->getDataFolder());		
-		if(!file_exists($this->getDataFolder()."config.yml")){
-			$this->saveResource('config.yml');	
-			}
-		$this->config = new Config($this->getDataFolder().'config.yml', Config::YAML);
-	}
+ public function onEnable(){
+     $this->getServer()->getPluginManager()->registerEvents($this, $this);
+     @mkdir($this->getDataFolder());		
+     if(!file_exists($this->getDataFolder()."config.yml")){
+         $this->saveResource('config.yml');	
+     }
+     $this->config = new Config($this->getDataFolder().'config.yml', Config::YAML);
+	 
+     if (!$this->isConfig()) {
+
+          $this->getServer()->getPluginManager()->disablePlugin("EnderChestSlots");
+	  $this->getLogger()->critical("The config.yml file is not good, please check if the slots are between 0 and 26");
+	     
+     }
+	 
+  }
 	
-	public function onOpenEnderchest(InventoryOpenEvent $e)
-        {
+  public function onOpenEnderchest(InventoryOpenEvent $e)
+  {
         $inv = $e->getInventory();
-        
-        if($this->config->get("enderchest-slots") <= 26 and $this->config->get("enderchest-slots") >= 0){
-        		if ($inv instanceof EnderChestInventory) {
-            $slots = $this->config->get("enderchest-slots");
-            while ($slots <= 26) {
-                $glass = Item::get(Item::GLASS_PANE, 0, 1);
-                $glass->setCustomName(" ");
-                $inv->setItem($slots, $glass);
-                $slots++;
+	  
+        if ($inv instanceof EnderChestInventory) {
+		
+	    if ($player->hasPermission("enderchest.staff.perm")) {
+
+                $this->setSlots($player, $this->config->get("enderchest-slots-staff"));
+
+            } else if ($player->hasPermission("enderchest.vip+.perm")) {
+
+                $this->setSlots($player, $this->config->get("enderchest-slots-vip+"));
+
+            } else if ($player->hasPermission("enderchest.vip.perm")) {
+
+                $this->setSlots($player, $this->config->get("enderchest-slots-vip"));
+
+            } else if ($player->hasPermission("enderchest.yt.perm")) {
+
+                $this->setSlots($player, $this->config->get("enderchest-slots-yt"));
+
+            }  else {
+
+                $this->setSlots($player, $this->config->get("enderchest-slots-default"));
+
             }
+		
         }
+	  
     }
-}
 
     public function onEnderchestTransaction(InventoryTransactionEvent $e)
     {
@@ -45,11 +68,62 @@ class Main extends PluginBase implements Listener {
         foreach ($transactions as $transaction) {
             foreach ($e->getTransaction()->getInventories() as $inv) {
                 if ($inv instanceof EnderChestInventory) {
-                    if($transaction->getSourceItem()->getName() == " "){
+                    if($transaction->getSourceItem()->getName() == " " &&
+		       $transaction->getSourceItem()->getId() == Item::STAINED_GLASS_PANE &&
+		       $transaction->getSourceItem()->getDamage() == 15
+		      or
+		       $transaction->getTargetItem()->getName() == " " &&
+		       $transaction->getTargetItem()->getId() == Item::STAINED_GLASS_PANE &&
+		       $transaction->getTargetItem()->getDamage() == 15)
+		    {
                         $e->setCancelled();
                     }
                 }
             }
         }
     }
+	
+    private function setSlots(Player $player,int $slots) : void 
+    {
+        while ($slots <= 26) {
+		
+	   if ($player->getEnderChestInventory()->getItem($slot)->getId() === Item::STAINED_GLASS_PANE &&
+               $player->getEnderChestInventory()->getItem($slot)->getDamage() === 15 &&
+               $player->getEnderChestInventory()->getItem($slot)->getCustomName() === " ") {
+
+               $player->getEnderChestInventory()->setItem($slot, Item::get(0, 0, 1), true);
+
+           } else {
+		   
+               $glass = Item::get(Item::STAINED_GLASS_PANE, 15, 1);
+               $glass->setCustomName(" ");
+               $player->getEnderChestInventory()->setItem($slots, $glass);
+               $slots++;
+		   
+	   }
+		
+         }
+	    
+    }
+	
+    private function isConfig () : bool
+    {
+         $cfg = $this->config->getAll();
+	 
+	 foreach ($cfg as $name => $number) {
+		 
+	    if($number > 26 and $number < 0){
+		    
+	    
+	        retrun "false";
+		break;
+               
+	    }
+
+	 }
+	    
+	 return "true";
+	    
+    }
+	
 }
